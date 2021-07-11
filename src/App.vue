da3b9eb3a5e98bc446e354bfd73bbe970299935c
OhSeongCheol <dhtmdcjf7@gmail.com>
2021-07-12 00:15:20 +0900

    Sidebar 샘플 추가 및 커밋 테스트

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <div id="app">
     <main-header/>
+    <side-header/>
     <router-view></router-view>
   </div>
 </template>
@@ -8,11 +9,13 @@
 
 <script>
 import mainHeader from './components/common/Header.vue'
+import sideHeader from './components/common/Sidebar.vue'
 
 export default {
   name: 'App',
   components: {
     mainHeader ,
+    sideHeader
   },
 }
 </script>
---
+++ src/components/common/Sidebar.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <b-sidebar id="sidebar-1" title="Sidebar" visible="true" z-index="-1">
+      <div class="px-3 py-2">
+        <p>
+          Cras mattis consectetur purus sit amet fermentum. Cras justo odio, dapibus ac facilisis
+          in, egestas eget quam. Morbi leo risus, porta ac consectetur ac, vestibulum at eros.
+        </p>
+        <b-img src="https://picsum.photos/500/500/?image=54" fluid thumbnail></b-img>
+      </div>
+    </b-sidebar>
+  </div>
+</template>
+
+<script>
+</script>
+
+<style scoped>
+</style>
