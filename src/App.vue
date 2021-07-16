db8f7f4e8eb3f775a31d24d0e3553e3038bb32c8
OhSeongCheol <dhtmdcjf7@gmail.com>
2021-07-16 23:45:20 +0900

    Common Header 템플릿 생성

--- src/App.vue
+++ src/App.vue
@@ -2,37 +2,28 @@
   
 <v-app>
   <v-navigation-drawer app clipped>
-    <!-- -->
   </v-navigation-drawer>
-
-  <v-app-bar app clipped-left='true'>
-    <!-- -->
-  </v-app-bar>
-
-  <!-- Sizes your content based upon application components -->
+  <common-header></common-header>
   <v-main>
-
-    <!-- Provides the application the proper gutter -->
     <v-container fluid>
-
-      <!-- If using vue-router -->
       <router-view></router-view>
     </v-container>
   </v-main>
-
   <v-footer app>
-    <!-- -->
   </v-footer>
 </v-app>
 </template>
 
 <script>
+
 // import HelloWorld from './components/HelloWorld';
+import CommonHeader from './components/common/Header.vue';
 
 export default {
   name: 'App',
 
   components: {
+    CommonHeader,
     // HelloWorld,
   },
 
--- src/components/common/Header.vue
+++ src/components/common/Header.vue
@@ -1,41 +1,30 @@
 <template>
-  <b-navbar toggleable="lg" type="dark" variant="info">
-    <router-link to="/">
-      <b-navbar-brand>
-       <h1>Crud-IO</h1>
-      </b-navbar-brand>
-    </router-link>
-    <router-link to="/posts">
-      <b-navbar-brand>Posts</b-navbar-brand>
-    </router-link>
+    <v-app-bar app clipped-left color="teal accent-3">
+    <v-btn
+      elevation="2"
+      fab
+      small
+    >
+      <v-icon>mdi-cursor-default</v-icon>
+    </v-btn>
 
-    <b-collapse id="nav-collapse" is-nav>
-      <!-- <b-navbar-nav>
-        <router-link to="/"> <b-nav-item href="#">foo </b-nav-item></router-link>
-        <b-nav-item href="#" disabled>Disabled</b-nav-item>
-        
-        <router-link to="bar">bar </router-link>
-      </b-navbar-nav> -->
+    <v-toolbar-title style="padding: 10px">
+      <h4>
+        Crudio
+      </h4>
+    </v-toolbar-title>
+    
+    <v-spacer></v-spacer>
+        <v-tabs align-with-title>
+          <v-tab>Posts</v-tab>
+          <v-tab>Tab 2</v-tab>
+          <v-tab>Tab 3</v-tab>
+        </v-tabs>
 
-<!-- 드롭박스 버튼 -->
-      <!-- <b-navbar-nav class="ml-auto">
-        <b-nav-item-dropdown text="Lang" right>
-          <b-dropdown-item href="#">EN</b-dropdown-item>
-          <b-dropdown-item href="#">ES</b-dropdown-item>
-          <b-dropdown-item href="#">RU</b-dropdown-item>
-          <b-dropdown-item href="#">FA</b-dropdown-item>
-        </b-nav-item-dropdown>
-
-        <b-nav-item-dropdown right>
-          <template v-slot:button-content>
-            <em>User</em>
-          </template>
-          <b-dropdown-item href="#">Profile</b-dropdown-item>
-          <b-dropdown-item href="#">Sign Out</b-dropdown-item>
-        </b-nav-item-dropdown>
-      </b-navbar-nav> -->
-    </b-collapse>
-  </b-navbar>
+    <v-btn icon>
+      <v-icon>mdi-magnify</v-icon>
+    </v-btn>
+  </v-app-bar>
 </template>
 
 <script>
@@ -46,14 +35,4 @@
 </script>
 
 <style scoped>
-header {
-  width: 100%;
-  height: 60px;
-  font-weight: bold;
-  color: white;
-  background-color: #7386D5;
-  align-items: center;
-  display: flex;
-  padding: 10px;
-}
 </style>
